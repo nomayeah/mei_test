15cac3b460e76bfecf608f019423d84e3e93fcd8
zap-ynonaka <y_nonaka@zappallas.com>
2018-03-26 08:58:44 +0900

    test

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var frontnote = require("gulp-frontnote");
+
+var browser = require("browser-sync");
+
+gulp.task("sass", function() {
+  gulp.src("sass/**/*scss")
+    .pipe(plumber())
+    .pipe(frontnote({
+        css: '../css/style.css'
+    }))
+    .pipe(sass({
+      outputStyle: 'compact'
+    }))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("./css"))
+    .pipe(browser.reload({stream:true}));
+});
+
+var uglify = require("gulp-uglify");
+gulp.task("js", function() {
+  gulp.src(["js/**/*.js","!js/min/**/*.js"])
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest("./js/min"))
+    .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+var imagemin = require("gulp-imagemin");
+gulp.task("imageMinTask", function() {
+  gulp.src("images/*.png")
+    .pipe(imagemin())
+    .pipe(gulp.dest("minified_images/"));
+});
+
+gulp.task("default", ['server'], function() {
+  gulp.watch(["js/**/*.js","!js/min/**/*.js"],["js"]);
+  gulp.watch("sass/**/*.scss",["sass"]);});
